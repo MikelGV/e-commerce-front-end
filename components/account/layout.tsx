11e2cf57557429bc04226c17bb976458f6b6b8d4
MikelGV <mikelgaldosv@gmail.com>
2021-11-21 14:12:02 +0100

    layout.tsx updated

--- components/account/layout.tsx
+++ components/account/layout.tsx
@@ -1,1 +1,20 @@
-import link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
+
+import { userService } from "services";
+
+export function Layout({children}) {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (userService.userValue) {
+            router.push('/');
+        }
+    }, []);
+    
+    return (
+        <div className="col-md-6 offset-md-3 mt-5">
+            {children}
+        </div>
+    );
+}

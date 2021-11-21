ee363f979d931f372303308e6bade032c6eef55c
MikelGV <mikelgaldosv@gmail.com>
2021-11-21 13:04:54 +0100

    start the work in the login

--- pages/account/login.tsx
+++ pages/account/login.tsx
@@ -1,1 +1,7 @@
-export function Login() {}
+import { useRouter } from "next/dist/client/router";
+import { useForm } from "react-hook-form";
+
+export default function Login() {
+    const router = useRouter();
+    
+}

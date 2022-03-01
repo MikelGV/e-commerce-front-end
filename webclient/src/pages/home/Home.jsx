ffd23523632cee991c31b1e22674837a1e154766
MikelGV <mikelgaldosv@gmail.com>
2022-03-01 03:38:43 +0100

    Products added to home page

--- webclient/src/pages/home/Home.jsx
+++ webclient/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Announcement from '../../components/Announcement/Announcement'
 import Categories from '../../components/Categories/Categories'
 import Navbar from '../../components/navbar/navbar'
+import Products from '../../components/Products/Products'
 import Slider from '../../components/Slider/Slider'
 
 
@@ -12,6 +13,7 @@ const Home = () => {
         <Navbar/>
         <Slider/>
         <Categories/>
+        <Products/>
     </div>
   )
 }

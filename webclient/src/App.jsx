23029df8f5e8ab18475023da6a1e5278e1c99893
MikelGV <mikelgaldosv@gmail.com>
2022-03-01 07:16:12 +0100

    imports

--- webclient/src/App.jsx
+++ webclient/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/home/Home";
+import Product from "./pages/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Product />
     </div>
   );
 }

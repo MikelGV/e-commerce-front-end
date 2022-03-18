379079fba0bf50a9a1767ec829db63f110ed8b65
MikelGV <mikelgaldosv@gmail.com>
2022-03-18 05:09:34 +0100

    a lot of things

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -12,35 +12,21 @@ import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User/>} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newproduct" element={<NewProduct/>} />
+        </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 

cb1a50466f495abc9257476f256da33963bd8d59
MikelGV <mikelgaldosv@gmail.com>
2022-03-22 05:43:07 +0100

    topbar and sidebar removed

--- admin/src/App.js
+++ admin/src/App.js
@@ -14,20 +14,16 @@ import Login from "./pages/login/Login";
 function App() {
   return (
     <BrowserRouter>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/user/:userId" element={<User/>} />
-          <Route path="/newUser" element={<NewUser />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/newproduct" element={<NewProduct/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/user/:userId" element={<User/>} />
+        <Route path="/newUser" element={<NewUser />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/newproduct" element={<NewProduct/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
